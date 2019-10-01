748747e9f2e74097a1b564c8eeec567407729936
Paula de Pinho <pauladepinho.rj@gmail.com>
2019-10-01 19:57:13 -0300

    Add files via upload
    Imprime"Hello World". Pega e imprime um nome. Calcula área de um retângulo. Calcula juros simples.

---
+++ HelloWorld.py
@@ -0,0 +1,29 @@
+#Imprime "Hello, World".
+
+#print ('Hello, World!')
+
+
+#Pergunta e imprime um nome.
+
+#name = input ('What\'s your name? ')
+#print ('Hello, ' + name + '!!!!!!!!!!!!')
+
+
+#Calcula área de um retângulo
+
+#base = float (input ('Qual é a base do retângulo? '))
+#altura = float (input ('E qual é a altura? '))
+
+#area = int (base * altura)
+
+#print ('A área do retângulo é ' + str (area) + '.')
+
+#Calcula juros simples. (juros = capital * taxa * tempo)
+
+capitalInicial = float (input ('Capital inicial: '))
+i = float (input ('Taxa: '))
+tempo = float (input ('Tempo: '))
+
+j = capitalInicial * i * tempo
+
+print ('Juros: ', j)
